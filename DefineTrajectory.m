bbb0847f6730ad06fa5e0e1fa46526322f2cbfb5
alessandro <alex.aveni00@gmail.com>
2025-02-22 13:07:56 +0100

    Modificato metodo di calcolo dei param controllo

--- DefineTrajectory.m
+++ DefineTrajectory.m
@@ -1,3 +1,17 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Define the trajectory of the robot end effector
+%
+% Inputs:
+% t: time
+% x0: initial x position
+% y0: initial y position
+% T: period of the trajectory
+%
+% Outputs:  
+% xt: x position of the end effector
+% yt: y position of the end effector
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function [xt, yt] = defineTrajectory(t, x0, y0, T)
     % Define the radius of the semicircle
     r = 0.7;
